aca60bcbb5a4c0b3f27959edd0fe7ad2c2c0e610
ywow-ai <masyaqin37@gmail.com>
2025-04-05 10:56:36 +0700

    chore: Initial project setup (.gitignore, .npmrc, package.json, etc.) (Project Setup: Added all initial files; Dependencies: Included necessary packages; Configuration: Set up ESLint, Prettier, and Vite)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { treaty } from '@elysiajs/eden';
+	import type { App } from './api/[...slugs]/+server';
+	import { page } from '$app/state';
+
+	const { api } = treaty<App>(page.url.origin);
+
+	onMount(() => {
+		const f = async () => {
+			try {
+				const r = await api.index.get();
+				console.log(r);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		f();
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ src/routes/api/[...slugs]/+server.ts
@@ -0,0 +1,15 @@
+import { Elysia, t } from 'elysia';
+
+const app = new Elysia({ prefix: '/api' })
+	.get('/', () => ({ status: 'ok', message: 'hello SvelteKit' }))
+	.post('/', ({ body }) => body, {
+		body: t.Object({
+			name: t.String()
+		})
+	});
+
+type RequestHandler = (v: { request: Request }) => Response | Promise<Response>;
+
+export const GET: RequestHandler = ({ request }) => app.handle(request);
+export const POST: RequestHandler = ({ request }) => app.handle(request);
+export type App = typeof app;
